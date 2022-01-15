6883f6d765c44a5702f68d44d7ce8d40fd7a7124
cabroderick <sedrahedron@gmail.com>
2022-01-15 15:18:18 -0500

    Amended normalize_classname to remove leading and trailing whitespace

--- train.py
+++ train.py
@@ -217,6 +217,7 @@ class CustomDataset(utils.Dataset):
 
   def normalize_classname(self, class_name): # normalize the class name to one used by the model
     class_name = class_name.lower() # remove capitalization
+    class_name = class_name.strip() # remove leading and trailing whitespace
     classes_dict = { # dictionary containing all class names used in labels and their appropriate model class name
       'gas entrapment porosity': 'gas entrapment porosity',
       'keyhole porosity': 'keyhole porosity',
@@ -225,7 +226,7 @@ class CustomDataset(utils.Dataset):
       'gas porosity': 'gas entrapment porosity',
       'lack-of-fusion': 'lack of fusion porosity',
       'keyhole': ' keyhole porosity',
-      'other': 'other'
+      'other': 'other',
     }
     return classes_dict.get(class_name)
 
