46b268114694120a6f782a3511b347b2081c2a38
cabroderick <sedrahedron@gmail.com>
2022-01-19 16:06:47 -0500

    Updated dataset_summary to generate csv and included csv

--- dataset_summary.py
+++ dataset_summary.py
@@ -1,34 +1,34 @@
-'''
-Creates summary of dataset by class, files, etc.
-Usage: python dataset_summary.py [optional output filename]
-'''
-
 import json
 import os
-import sys
 from normalize_classnames import normalize_classname
+import csv
 
-if len(sys.argv) > 1:
-    sys.stdout = open(sys.argv[1], 'w')
+csv_rows = ['Dataset', '# of images', 'lack of fusion porosity', 'gas entrapment porosity', 'keyhole porosity', 'other', 'total annotation instances']
+csv_data = []
 
 BASE_IMAGES_DIR = './Data/Images/' # directory where all images can be found
 BASE_ANNOTATIONS_DIR = './Data/Labels/' # directory where all images labels can be found
-IMAGES_DIRS = ['G0/', 'G9/', 'H0/', 'H4/', 'H5/', 'H6/', 'H8/', 'H9/', 'J0/', 'J1/', 'J3/', 'J4/', 'J7/',
-                 'J8/', 'K0/', 'Q0/', 'Q3/', 'Q5/', 'Q9/', 'R2/', 'R6/', 'R7/'] # list of directories where images are contained
-ANNOTATIONS_DIRS = ['Labeled G0/', 'Labeled G9/', 'Labeled H0/', 'Labeled H4/', 'Labeled H5/', 'Labeled H6/',
+IMAGES_DIRS = ['G0/', 'G3/', 'G9/', 'H0/', 'H4/', 'H5/', 'H6/', 'H8/', 'H9/', 'J0/', 'J1/', 'J3/', 'J4/', 'J7/',
+                 'J8/', 'K0/', 'K4/', 'Q0/', 'Q3/', 'Q5/', 'Q9/', 'R0/', 'R2/', 'R6/', 'R7/']
+# corresponding list of directories where annotations are contained
+ANNOTATIONS_DIRS = ['Labeled G0/', 'Labeled G3/', 'Labeled G9/', 'Labeled H0/', 'Labeled H4/', 'Labeled H5/', 'Labeled H6/',
                       'Labeled H8/', 'Labeled H9/', 'Labeled J0/', 'Labeled J1/', 'Labeled J3/', 'Labeled J4/',
-                      'Labeled J7/', 'Labeled J8/', 'Labeled K0/', 'Labeled Q0/', 'Labeled Q3/', 'Labeled Q5/',
-                      'Labeled Q9/', 'Labeled R2/', 'Labeled R6/', 'Labeled R7/'] # corresponding list of directories where annotations are contained
-
-classes_all = {}
+                      'Labeled J7/', 'Labeled J8/', 'Labeled K0/', 'Labeled K4/', 'Labeled Q0/', 'Labeled Q3/', 'Labeled Q5/',
+                      'Labeled Q9/', 'Labeled R0/', 'Labeled R2/', 'Labeled R6/', 'Labeled R7/']
 
 for i in range(len(IMAGES_DIRS)):
+    row = []
     image_dir = BASE_IMAGES_DIR + IMAGES_DIRS[i]
     annotation_dir = BASE_ANNOTATIONS_DIR + ANNOTATIONS_DIRS[i]
-    print('Dataset: ' + IMAGES_DIRS[i][:-1])
-    print('Total images: ' + str(len(os.listdir(image_dir))))
+    row.append(IMAGES_DIRS[i][:-1])
+    row.append(str(len(os.listdir(image_dir))))
 
-    classes = {} # dictionary containing the classes for a particular dataset
+    classes = {'lack of fusion porosity': 0,
+     'gas entrapment porosity': 0,
+     'keyhole porosity': 0,
+     'other': 0}
+
+    total_annotations = 0
 
     for annotation in os.listdir(annotation_dir):
         path = annotation_dir + annotation
@@ -36,26 +36,17 @@ for i in range(len(IMAGES_DIRS)):
         annotation_json = json.load(f_ann)
         for shape in annotation_json['shapes']:
             label = normalize_classname(shape['label'])
-            if label in classes.keys():
-                classes[label] += 1
-            else:
-                classes[label] = 1
+            classes[label] += 1
 
-    print('Number of annotation classes: ' + str(len(classes.keys())))
-    print('Annotation occurrences:')
     for label in classes.keys():
-        print(label + ': ' + str(classes.get(label)) + ' occurrences')
-    print('=========================================')
+        total_annotations += classes.get(label)
+        row.append(classes.get(label))
 
-    for key in classes.keys():
-        if key in classes_all.keys():
-            classes_all[key] += classes.get(key)
-        else:
-            classes_all[key] = classes.get(key)
+    row.append(total_annotations)
+    csv_data.append(row)
 
-print('Total annotation occurrences:')
-for label in classes_all.keys():
-    print(label + ': ' + str(classes_all.get(label)) + ' occurrences')
-
-if len(sys.argv) > 1:
-    sys.stdout.close()
+with open('dataset_summary.csv', 'w') as f:
+    # using csv.writer method from CSV package
+    write = csv.writer(f)
+    write.writerow(csv_rows)
+    write.writerows(csv_data)
