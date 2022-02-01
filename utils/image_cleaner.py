19a0023ce7f5200d5dc13fb9a8a66fd83ce01c11
cabroderick <sedrahedron@gmail.com>
2022-01-31 20:04:31 -0500

    Added image_cleaner.py

---
+++ utils/image_cleaner.py
@@ -0,0 +1,21 @@
+'''
+Removes 20X_YZ at the end of image names
+'''
+
+import cv2
+import os
+
+ROOT_DIR = '../Data/Images/' # root directory where all JSONs are contained
+SUB_DIRS = ['H0', 'J1'] # subdirs containing JSONs (remove 'Labeled' from dir name)
+
+for dir in SUB_DIRS:
+    dir_path = ROOT_DIR + dir + '/'
+    for file in os.listdir(dir_path):
+        img_path = ROOT_DIR + dir + '/' + file
+        if img_path[-11:] == '_20X_YZ.tif':
+            img = cv2.imread(img_path)
+            os.remove(img_path)
+            img_path = img_path[:-11] + img_path[-4:]
+            cv2.imwrite(img_path, img)
+
+
