4bfc958fdca4a9387f3c19e2524bcbffb80aeeaa
cabroderick <sedrahedron@gmail.com>
2022-01-31 19:46:04 -0500

    Finished json_cleaner.py

--- utils/json_cleaner.py
+++ utils/json_cleaner.py
@@ -1,5 +1,6 @@
 '''
 JSON cleaner - removes unwanted classes
+Replace SUB_DIRS with the appropriate annotation directories and you're set!
 '''
 
 import os
@@ -11,25 +12,24 @@ SUB_DIRS = ['H6'] # subdirs containing JSONs (remove 'Labeled' from dir name)
 
 for dir in SUB_DIRS:
     dir_path = ROOT_DIR + 'Labeled ' + dir + '/'
-    for ann_path in os.listdir(dir_path):
-        ann_path = ROOT_DIR + 'Labeled ' + dir + '/' + ann_path
+    for file in os.listdir(dir_path):
+        ann_path = ROOT_DIR + 'Labeled ' + dir + '/' + file
         print(ann_path)
         with open(ann_path, 'r') as f_ann: # read JSON
             annotation_json = json.load(f_ann)
-
-        # start with empty shapes array, parse through, remove gas entrapment, change class name, replace at the end
         # rename filenames to remove 20X_YZ.json
 
-        for i in range(len(annotation_json['shapes'])):
-            if normalize_classname.normalize_classname(annotation_json['shapes'][i]['label']) == 'gas entrapment porosity':
-                print('gas entrapment')
-                annotation_json['shapes'].pop(i)
-
-        # [annotation_json['shapes'].pop(annotation_json['shapes'].index(shape)) for shape in annotation_json['shapes']
-        #  if normalize_classname.normalize_classname(shape['label']) == 'gas entrapment porosity'] # TODO fix this
-
+        shapes = []
         for shape in annotation_json['shapes']:
             shape['label'] = normalize_classname.normalize_classname(shape['label'])
+            if shape['label'] != 'gas entrapment porosity':
+                shapes.append(shape)
+
+        os.remove(ann_path) # delete old ann path
+        if file[-12:] == '_20X_YZ.json':
+            ann_path = ann_path[:-12] + ann_path[-5:] # remove 20X_YZ at the end of annotations
+
+        annotation_json['shapes'] = shapes
 
         with open(ann_path, 'w') as f_ann: # write back to the JSON
             json.dump(annotation_json, f_ann, indent=2)
