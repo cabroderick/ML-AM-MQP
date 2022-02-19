0c3d9da775fe4841414ae0c69b51db7e8a04e850
cabroderick <sedrahedron@gmail.com>
2022-02-19 15:56:57 -0500

    Added load_dataset_paths.py

--- utils/json_cleaner.py
+++ utils/json_cleaner.py
@@ -5,30 +5,36 @@ Replace SUB_DIRS with the appropriate annotation directories and you're set!
 
 import os
 import json
-import normalize_classname
+from utils.normalize_classname import normalize_classname
+from utils.load_dataset_paths import load_dataset_paths
 
-ROOT_DIR = '../Data/Labels/' # root directory where all JSONs are contained
-SUB_DIRS = ['J4'] # subdirs containing JSONs (remove 'Labeled' from dir name)
+ROOT_IMG_DIR = '../Data/Images/' # root directory where all JSONs are contained
+ROOT_ANN_DIR = '../Data/Labels/'
+SUB_DIRS = ['G7'] # subdirs containing JSONs (remove 'Labeled' from dir name)
 
-for dir in SUB_DIRS:
-    dir_path = ROOT_DIR + 'Labeled ' + dir + '/'
-    for file in os.listdir(dir_path):
-        ann_path = ROOT_DIR + 'Labeled ' + dir + '/' + file
-        print(ann_path)
-        with open(ann_path, 'r') as f_ann: # read JSON
-            annotation_json = json.load(f_ann)
+img_dirs, ann_dirs = load_dataset_paths(ROOT_IMG_DIR, ROOT_ANN_DIR, SUB_DIRS)
 
-        shapes = []
-        for shape in annotation_json['shapes']:
-            shape['label'] = normalize_classname.normalize_classname(shape['label'])
-            if shape['label'] != 'gas entrapment porosity' and shape['label'] != 'other':
-                shapes.append(shape)
+for i in range(len(ann_dirs)):
+    ann_path = ann_dirs[i]
+    img_path = img_dirs[i]
+    print(ann_path)
+    with open(ann_path, 'r') as f_ann:  # read JSON
+        annotation_json = json.load(f_ann)
 
-        os.remove(ann_path) # delete old ann path
-        if file[-12:].replace('-', '_').lower() == '_20x_yz.json':
-            ann_path = ann_path[:-12] + ann_path[-5:] # remove 20X_YZ at the end of annotations
+    shapes = []
+    for shape in annotation_json['shapes']:
+        shape['label'] = normalize_classname(shape['label'])
+        if shape['label'] != 'gas entrapment porosity' and shape['label'] != 'other':
+            shapes.append(shape)
+
+    os.remove(ann_path)  # delete old ann path
+
+    if shapes:  # only generate new JSON if there are any annotations left
+        if ann_path.replace('-', '_').lower() == '_20x_yz.json':
+            ann_path = ann_path[:-12] + ann_path[-5:]  # remove 20X_YZ at the end of annotations
 
         annotation_json['shapes'] = shapes
-
-        with open(ann_path, 'w') as f_ann: # write back to the JSON
+        with open(ann_path, 'w') as f_ann:  # write back to the JSON
             json.dump(annotation_json, f_ann, indent=2)
+    else:
+        os.remove(img_path) # remove image associated with json if it has no valid labels
---
+++ utils/load_dataset_paths.py
@@ -0,0 +1,18 @@
+import os
+
+def load_dataset_paths(root_img_dir, root_annotation_dir, dirs):
+    image_paths = []
+    annotation_paths = []
+
+    for i in range(len(dirs)):
+        i_dir = root_img_dir + dirs[i] + '/'
+        a_dir = root_annotation_dir + 'Labeled ' + dirs[i] + '/'
+        for file in os.listdir(i_dir):
+            i_id = file[:-4]
+            image_paths.append(i_dir + i_id + '.tif')
+            annotation_paths.append(a_dir + i_id + '.json')
+
+    if len(image_paths) == len(annotation_paths):
+        return image_paths, annotation_paths
+    else:
+        return None, None
